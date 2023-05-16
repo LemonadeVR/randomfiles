fbe47b644d071319b429a104623d44a9c22030e0
LemonadeVR <104174925+LemonadeVR@users.noreply.github.com>
2023-05-16 08:33:42 -0600

    Update rotateboard.js

--- rotateboard.js
+++ rotateboard.js
@@ -1,11 +1,11 @@
 const elements = document.getElementsByClassName('VFlF2c EjCLSb');
-    let rotation = 0;
+let rotation = 0;
 
-    document.addEventListener('keydown', (event) => {
-        rotation += 5;
-        for (let i = 0; i < elements.length; i++) {
-            let element = elements[i];
-            element.style.transform = `rotate(${rotation}deg)`;
-            element.style.transition = "0.1s";
-        }
-    })
+document.addEventListener('keydown', (event) => {
+    rotation += 5;
+    for (let i = 0; i < elements.length; i++) {
+        let element = elements[i];
+        element.style.transform = `rotate(${rotation}deg)`;
+        element.style.transition = "0.1s";
+    }
+})
